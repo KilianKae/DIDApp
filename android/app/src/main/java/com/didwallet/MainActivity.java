23842e0d46b8e0504a512a493cabf2b59d736457
Kilian Käslin <kilian.kaeslin@quartett-mobile.de>
2020-04-13 17:35:10 +0200

    - Renamed app - Updated react-native

--- android/app/src/main/java/com/didwallet/MainActivity.java
+++ android/app/src/main/java/com/didwallet/MainActivity.java
@@ -1,4 +1,4 @@
-package com.didapp;
+package com.didwallet;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "DIDApp";
+    return "DIDWallet";
   }
 }
--- android/app/src/main/java/com/didwallet/MainApplication.java
+++ android/app/src/main/java/com/didwallet/MainApplication.java
@@ -1,4 +1,4 @@
-package com.didapp;
+package com.didwalletwallet;
 
 import android.app.Application;
 import android.content.Context;

642201e4893b6089a3db8622af4bd07b67642688
Kilian Käslin <kilian.kaeslin@quartett-mobile.de>
2020-05-01 12:31:59 +0200

    - Fixed style

--- src/Components/DeleteButton.js
+++ src/Components/DeleteButton.js
@@ -1,29 +1,31 @@
 import React, {Component} from 'react';
 import {Icon} from 'native-base';
-import {TouchableOpacity} from 'react-native';
+import {TouchableOpacity, StyleSheet} from 'react-native';
 
 export default class DeleteButton extends Component {
   render() {
     return (
-      <TouchableOpacity
-        style={[
-          {
-            flex: 1,
-            alignItems: 'flex-end',
-            justifyContent: 'center',
-            paddingRight: 25,
-            marginRight: 10,
-            marginTop: 6,
-            marginBottom: 6,
-            backgroundColor: 'red',
-          },
-        ]}>
+      <TouchableOpacity style={this.styles.background}>
         <Icon
           name="md-remove-circle-outline"
-          style={[{color: 'white'}]}
+          style={this.styles.icon}
           onPress={() => this.props.handleDelete()}
         />
       </TouchableOpacity>
     );
   }
+
+  styles = StyleSheet.create({
+    background: {
+      flex: 1,
+      alignItems: 'flex-end',
+      justifyContent: 'center',
+      paddingRight: 25,
+      marginRight: 10,
+      marginTop: 6,
+      marginBottom: 6,
+      backgroundColor: 'red',
+    },
+    icon: {color: 'white'},
+  });
 }
